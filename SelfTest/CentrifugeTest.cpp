7e5509c3fa0737f4135b72cf0b44ab08523c9f88
BOOK-0CD3P8P8FE\endur <endurodave@gmail.com>
2026-01-01 10:25:18 -0800

    Fix build

--- SelfTest/CentrifugeTest.cpp
+++ SelfTest/CentrifugeTest.cpp
@@ -56,7 +56,7 @@ STATE_DEFINE(CentrifugeTest, Idle, NoEventData)
 	SelfTest::ST_Idle(data);	
 
 	// Unregister for timer callbacks 
-	m_pollTimer.Expired = 0;
+	(*m_pollTimer.Expired) -= MakeDelegate(this, &CentrifugeTest::Poll, SelfTestEngine::GetInstance().GetThread());
 	m_pollTimer.Stop();
 }
 
@@ -68,7 +68,7 @@ STATE_DEFINE(CentrifugeTest, StartTest, StartData)
 	SelfTestEngine::InvokeStatusCallback("CentrifugeTest::ST_StartTest");
 
 	// Register for timer callbacks 
-	m_pollTimer.Expired = MakeDelegate(this, &CentrifugeTest::Poll, SelfTestEngine::GetInstance().GetThread());
+	(*m_pollTimer.Expired) += MakeDelegate(this, &CentrifugeTest::Poll, SelfTestEngine::GetInstance().GetThread());
 
 	InternalEvent(ST_ACCELERATION);
 }
